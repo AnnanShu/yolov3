15fe8c86676bd3fd34ac25b3040994094e39c90a
AnnanShu <shuannan@126.com>
2021-03-06 17:50:24 +0800

    windows 05

--- models/experimental.py
+++ models/experimental.py
@@ -10,10 +10,25 @@ class CrossConv(nn.Module):
     # Cross convolution downsampling
     def __init__(self, c1, c2, k=3, s=1, g=1, e=1.0, shortcut=False):
         super(CrossConv, self).__init__()
-        pass
+        c_ = int(c2 * e)
+        self.cv1 = Conv(c1, c_, (1, k), (1, s))
+        self.cv2 = Conv(c_, c2, (k, 1), (s, 1))
+        self.add = shortcut and c1 == c2
 
     def forward(self, x):
-        return x
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+
+class Sum(nn.Module):
+    # weight sum of 2 or more layers
+    def __init__(self, n, weight=False):
+        super(Sum, self).__init__()
+        self.weight = weight  # apply weight boolean
+        self.iter = range(n - 1)  # iter object
+        if weight:
+            self.w = nn.Parameter(-torch.arange(1., n) / 2, requires_grad=True)  # layer weight
+
+
 
 class MixConv2d:
     pass
--- yaml_test.py
+++ yaml_test.py
@@ -1,6 +1,6 @@
 import yaml
 
-yaml_path = "./models/yolov3-spp.yaml"
+yaml_path = r"./models/yolov3-spp.yaml"
 
 with open(yaml_path, 'r') as f:
     yaml_file = yaml.load(f, yaml.FullLoader)
@@ -8,11 +8,10 @@ with open(yaml_path, 'r') as f:
 ch = yaml_file['ch'] = yaml_file.get('ch', 3)
 
 anchors = yaml_file['anchors']
-type(anchors)
+print(type(anchors))
 
 import torch.nn as nn
 
 
-eval('nn.Conv2d')(3, 3, 1, 1)
+print(eval('nn.Conv2d')(3, 3, 1, 1))
 
-eval
